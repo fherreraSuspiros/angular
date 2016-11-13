b92df2a8558bdba6a91bb0bcf58b4292362f70a2
fherreraSuspiros <sistemas2@suspirospastelerias.com>
2016-11-13 03:16:39 -0600

    practicas

--- js/core.js
+++ js/core.js
@@ -1,2 +1,11 @@
 var app = angular.module('app', []);
 
+app.controller('miCtrl', ['$scope', function ($scope)
+{
+  $scope.mensaje = 'Mensaje desde el controlador';
+  $scope.logPadre = function()
+  {
+    alert()
+  }
+
+}]);
